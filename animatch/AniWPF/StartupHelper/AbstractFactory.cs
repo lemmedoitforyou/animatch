7d9c49a05657ce6292d169535a1f3a085db98215
Yuliia Palamar <yuliyapalamar@gmail.com>
2023-11-11 01:05:43 +0200

    added stylecop

--- animatch/AniWPF/StartupHelper/AbstractFactory.cs
+++ animatch/AniWPF/StartupHelper/AbstractFactory.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AniWPF.StartupHelper
 {
     public class AbstractFactory<T> : IAbstractFactory<T>
     {
-        public readonly Func<T> _factory;
+        private readonly Func<T> factory;
 
         public AbstractFactory(Func<T> factory)
         {
-            _factory = factory;
+            this.factory = factory;
         }
 
         public T Create()
         {
-            return _factory();
+            return this.factory();
         }
     }
 }
