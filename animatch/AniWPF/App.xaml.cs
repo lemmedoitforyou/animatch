f4cc77342438348e25feff67f151724310788589
Yuliia Palamar <yuliyapalamar@gmail.com>
2023-11-14 10:36:54 +0200

    lods

--- animatch/AniWPF/App.xaml.cs
+++ animatch/AniWPF/App.xaml.cs
@@ -7,6 +7,7 @@ using AniDAL.Repositories;
 using AniWPF.StartupHelper;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Serilog;
 
 namespace AniWPF;
@@ -14,17 +15,18 @@ namespace AniWPF;
 public partial class App : Application
 {
     public static IHost? AppHost { get; private set; }
+    private readonly ILogger<App> _logger;
 
     public App()
     {
         AppHost = Host.CreateDefaultBuilder()
-            //.UseSerilog((host, loggerConfiguration) =>
-            //{
-            //    loggerConfiguration.WriteTo.File("test.txt", rollingInterval: RollingInterval.Day)
-            //        .WriteTo.Debug()
-            //        .MinimumLevel.Error()
-            //        .MinimumLevel.Override("LoggingDemo.Commands", Serilog.Events.LogEventLevel.Debug);
-            //})
+           .UseSerilog((host, loggerConfiguration) =>
+           {
+               loggerConfiguration.WriteTo.File("logs\\test.txt", rollingInterval: RollingInterval.Day)
+                   .WriteTo.Debug()
+                   .MinimumLevel.Error()
+                   .MinimumLevel.Override("LoggingDemo.Commands", Serilog.Events.LogEventLevel.Debug);
+           })
             .ConfigureServices((hostContext, services) =>
             {
                 services.AddSingleton<LogInWindow>();
@@ -38,6 +40,8 @@ public partial class App : Application
                 services.AddTransient<IUserService, UserService>();
                 services.AddTransient<ApplicationDbContext>();
             }).Build();
+
+        _logger = AppHost.Services.GetRequiredService<ILogger<App>>();
     }
 
     protected override async void OnStartup(StartupEventArgs e)
